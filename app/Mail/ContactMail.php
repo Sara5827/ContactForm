be785fa27bbc60d3c54e5ee7439b7ccad5272e60
ouldjelloulsara@gmail.com <ouldjelloulsara@gmail.com>
2021-03-21 20:04:19 +0100

    create a page contact email to show the informaion

--- app/Mail/ContactMail.php
+++ app/Mail/ContactMail.php
@@ -11,14 +11,18 @@ class ContactMail extends Mailable
 {
     use Queueable, SerializesModels;
 
+    public $details;
+
+
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct($details)
     {
-        //
+        $this->details = $details;
     }
 
     /**
@@ -28,6 +32,6 @@ class ContactMail extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->subject('Contact Message')->view('emails.ContactMail');
     }
 }
